47d5d943528119a71dbf33a445b92470c47438aa
jhmcclellandii <jhmcclellandii@gmail.com>
2015-06-13 21:08:39 -0500

    Initial Commit

---
+++ TheWalls/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheWalls
+//
+//  Created by John McClelland on 6/13/15.
+//  Copyright (c) 2015 machine^n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TheWalls/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TheWalls
+//
+//  Created by John McClelland on 6/13/15.
+//  Copyright (c) 2015 machine^n. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
