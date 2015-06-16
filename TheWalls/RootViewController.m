32783e0a73032817ddf6505e751651b6504152ec
jhmcclellandii <jhmcclellandii@gmail.com>
2015-06-15 21:17:55 -0500

    added photo subclass

--- TheWalls/RootViewController.m
+++ TheWalls/RootViewController.m
@@ -8,15 +8,23 @@
 
 #import "RootViewController.h"
 #import "UIColor+CustomColors.h"
+#import "Photo.h"
 
 #import <MapKit/MapKit.h>
 #import <CoreLocation/CoreLocation.h>
 
-@interface RootViewController () <MKMapViewDelegate, CLLocationManagerDelegate>
+@interface RootViewController () <MKMapViewDelegate, CLLocationManagerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+//Location properties
 @property CLLocationManager *locationManager;
-//@property MKUserLocation *userLocation;
+@property CLLocation *userLocation;
 @property MKMapView *primaryMapView;
 
+//Photo properties
+@property UIImage *imageDidSelected;
+
+
+//Button properties
 @property UIDynamicAnimator *dynamicAnimator;
 @property UIButton *soundButton;
 @property UIButton *photoButton;
@@ -48,6 +56,7 @@
     self.drawButton =  [self createButtonWithTitle:@"D" chooseColor:[UIColor peonyColor]];
     self.mainButton =  [self createButtonWithTitle:@"M" chooseColor:[UIColor peonyColor]];
     [self.mainButton addTarget:self action:@selector(fanButtons:) forControlEvents:UIControlEventTouchUpInside];
+    [self.photoButton addTarget:self action:@selector(onPhotoButtonPressed) forControlEvents:UIControlEventTouchUpInside];
 
     //Future gestures stuff
     [self leftSwipeGestureInitialization];
@@ -88,13 +97,26 @@
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
     for (CLLocation *location in locations) {
         if (location.verticalAccuracy < 1000 && location.horizontalAccuracy < 1000) {
-            //Reverse GeoCode would go here
             [self.locationManager stopUpdatingLocation];
             NSLog(@"3");
         }
         NSLog(@"2");
     }
     NSLog(@"1");
+}
+
+-(void)reverseGeoCode:(CLLocation *)location {
+    CLGeocoder *geoCoder = [CLGeocoder new];
+    [geoCoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        CLPlacemark *placemark = placemarks.firstObject;
+        self.userLocation = placemark.location;
+    }];
+}
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
+    CLLocationCoordinate2D location = [userLocation coordinate];
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(location, 1000, 1000);
+    [self.primaryMapView setRegion:region animated:YES];
 }
 
 #pragma mark - Swipe Gestures
@@ -166,7 +188,7 @@
     [[UIDevice currentDevice]endGeneratingDeviceOrientationNotifications];
 }
 
-#pragma mark - Programmatic buttons
+#pragma mark - Create buttons
 //Need to subclass each button - draw, photo, audio
 
 - (UIButton *)createButtonWithTitle:(NSString *)title chooseColor:(UIColor *)color {
@@ -185,6 +207,8 @@
     return button;
 }
 
+#pragma mark - Main button
+
 - (void)fanButtons:(id)sender{
     [self.dynamicAnimator removeAllBehaviors];
     if (self.areButtonsFanned) {
@@ -198,7 +222,6 @@
 
 - (void)switchMainButtonState {
     //trying to change the state when pressed
-//    [self.mainButton.layer setBackgroundColor:(__bridge CGColorRef)([UIColor clearColor])];
     [self.mainButton.layer setBorderColor:(__bridge CGColorRef)([UIColor peonyColor])];
     [self.mainButton.layer setBorderWidth:5.0];
 }
@@ -232,5 +255,56 @@
     snapBehavior = [[UISnapBehavior alloc]initWithItem:self.drawButton snapToPoint:self.mainButton.center];
     [self.dynamicAnimator addBehavior:snapBehavior];
 }
+
+#pragma mark - Photo button
+
+- (void)onPhotoButtonPressed {
+    NSLog(@"Photo button pressed!");
+    [self takePhoto];
+}
+
+- (void)takePhoto {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    [self presentViewController:picker animated:YES completion:^{
+    }];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    self.imageDidSelected = info[UIImagePickerControllerEditedImage];
+    //Size image with button
+    CGSize size = self.photoButton.frame.size;
+    UIGraphicsBeginImageContext(size);
+    [self.imageDidSelected drawInRect:CGRectMake(0, 0, size.width, size.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    self.photoButton.backgroundColor = [UIColor colorWithPatternImage:newImage];
+    //
+    [picker dismissViewControllerAnimated:YES completion:^{
+        NSLog(@"Woot!");
+        [self savePhoto];
+    }];
+
+}
+
+- (void) savePhoto{
+    NSData *imageData = UIImagePNGRepresentation(self.imageDidSelected);
+    Photo *newPhoto = [Photo new];
+    newPhoto.imagePhoto = [PFFile fileWithName:@"image.png" data:imageData];
+//    newPhoto.caption = @"Photo";
+    newPhoto.latitude = self.userLocation.coordinate.latitude;
+    newPhoto.longitude = self.userLocation.coordinate.longitude;
+    [newPhoto setObject:[PFUser currentUser] forKey:@"createdBy"];
+    [newPhoto saveInBackground];
+
+    //Reset
+//    self.photoPreview.image = nil;
+//    self.captionText.text = nil;
+}
+
+
+
 
 @end
