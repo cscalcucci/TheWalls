b2abd82eb91cf7da43de5d9ba1740940be88cf66
Bryon Finke <bryon.finke@gmail.com>
2015-06-17 13:48:39 -0500

    username step view

--- TheWalls/UsernameViewController.m
+++ TheWalls/UsernameViewController.m
@@ -17,7 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    self.usernameTextField.placeholder = @"create a username";
 }
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
