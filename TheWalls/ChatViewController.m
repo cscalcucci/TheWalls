7ad71acd5f01d8fc3c6d1f2cae61930d0f52cdf2
cscalcucci <cscal@aphelion-enterprises.com>
2015-06-19 12:06:40 -0500

    Added activity class

---
+++ TheWalls/ChatViewController.m
@@ -0,0 +1,24 @@
+//
+//  ChatViewController.m
+//  TheWalls
+//
+//  Created by Christopher Scalcucci on 6/19/15.
+//  Copyright (c) 2015 machine^n. All rights reserved.
+//
+
+#import "ChatViewController.h"
+
+@interface ChatViewController ()
+
+@end
+
+@implementation ChatViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
