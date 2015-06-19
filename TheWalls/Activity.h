7e63365c48f4d0d788f0dba27621ee268dd58186
Bryon Finke <bryon.finke@gmail.com>
2015-06-19 13:45:08 -0500

    stash

---
+++ TheWalls/Activity.h
@@ -0,0 +1,22 @@
+//
+//  Activity.h
+//  TheWalls
+//
+//  Created by Christopher Scalcucci on 6/19/15.
+//  Copyright (c) 2015 machine^n. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+
+
+@interface Activity : PFObject<PFSubclassing>
+
++ (NSString *)parseClassName;
+
+@property PFUser *fromUser;
+@property PFUser *toUser;
+@property NSString *type;
+@property PFObject *content;
+
+@end
