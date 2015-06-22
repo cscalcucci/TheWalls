b503cc23924b3c6536db769765afd8063981fdd5
Bryon Finke <bryon.finke@gmail.com>
2015-06-22 13:49:18 -0500

    feed viewcontroller

---
+++ TheWalls/FeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedViewController.h
+//  TheWalls
+//
+//  Created by Bryon Finke on 6/22/15.
+//  Copyright (c) 2015 machine^n. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedViewController : UIViewController
+
+@end
---
+++ TheWalls/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  
+//
+//  Created by Bryon Finke on 6/22/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
